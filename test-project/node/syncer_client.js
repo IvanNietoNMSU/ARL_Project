ae8980e0b032b692966087f683111f4381ff5f46
Austin <austinmbos@gmail.com>
2020-04-17 11:25:41 -0600

    updated grpc code, moving to callable modules

--- test-project/node/syncer_client.js
+++ test-project/node/syncer_client.js
@@ -1,3 +1,5 @@
+const sqlite3 = require("sqlite3").verbose();
+
 
 var PROTO_PATH = __dirname + '/syncer.proto';
 
@@ -13,13 +15,101 @@ var packageDefinition = protoLoader.loadSync(
     });
 var syncer_proto = grpc.loadPackageDefinition(packageDefinition).syncer;
 
+const {
+	queryTable
+} = require("./databaseController");
+
+module.exports = {
+	syncData: async => {
+
+	  // start up grpc connection to the server
+	  var client = new syncer_proto.DatabaseSync('localhost:50051',
+										   grpc.credentials.createInsecure());
+
+	  let db = new sqlite3.Database(
+		  "./P1.db",
+		  sqlite3.OPEN_READ,
+		  err => {
+			  if (err) console.log(err);
+		  }
+	  );
+
+	  let results = [];
+	  let promise = new Promise((resolve, reject) => {
+		  db.all("SELECT * FROM findings;", [], (err, rows) => {
+			  if (err) {
+				  console.log("error blah")
+				  return err;
+			  }
+				  console.log("Sending data over!")
+			  rows.map(item => {
+
+				  // send entry to server through grpc connection
+				  // build the grpc object from the fields pulled out of the db
+				  // entry
+				  // status is useless right now, but it is nice to return something
+				  client.syncData({taskId: item.taskId,
+									type: item.type,
+									title: item.title,
+									assignedTo: item.assignedTo,
+									status: item.status,
+									description: item.description}, function(err, response) {
+					console.log('Status:', response.status);
+				  });
+
+			  });
+		  });
+		  db.close();
+	  });
+
+
+	},
+
+};
+
 function main() {
+
+	// start up grpc connection to the server
   var client = new syncer_proto.DatabaseSync('localhost:50051',
                                        grpc.credentials.createInsecure());
-  var user;
-  client.syncData({input: "blah"}, function(err, response) {
-    console.log('Status:', response.status);
+
+
+  let db = new sqlite3.Database(
+	  "./P1.db",
+	  sqlite3.OPEN_READ,
+	  err => {
+		  if (err) console.log(err);
+	  }
+  );
+
+  let results = [];
+  let promise = new Promise((resolve, reject) => {
+	  db.all("SELECT * FROM findings;", [], (err, rows) => {
+		  if (err) {
+			  console.log("error blah")
+			  return err;
+		  }
+			  console.log("Sending data over!")
+		  rows.map(item => {
+
+			  // send entry to server through grpc connection
+			  // build the grpc object from the fields pulled out of the db
+			  // entry
+			  // status is useless right now, but it is nice to return something
+			  client.syncData({taskId: item.taskId,
+			  					type: item.type,
+			  					title: item.title,
+			  					assignedTo: item.assignedTo,
+			  					status: item.status,
+			  					description: item.description}, function(err, response) {
+				console.log('Status:', response.status);
+			  });
+
+		  });
+	  });
+	  db.close();
   });
+
 }
 
-main();
+//main();
--- test-project/node/syncer_server.js
+++ test-project/node/syncer_server.js
@@ -14,15 +14,30 @@ var packageDefinition = protoLoader.loadSync(
 var syncer_proto = grpc.loadPackageDefinition(packageDefinition).syncer;
 
 function syncData(call, callback) {
-  console.log("[*] Recieved some data to store... ")
+  console.log("[*] Recieved some data to store [*]")
+  console.log("[type]        " + call.request.type)
+  console.log("[title]       " + call.request.title)
+  console.log("[assignedTo]  " + call.request.assignedTo)
+  console.log("[status]      " + call.request.status)
+  console.log("[description] " + call.request.description)
+  console.log("[*] End recieved data [*]")
   callback(null, {status: 200});// + call.request.name});
 }
+
+module.exports = {
+	syncData: async => {
+	  var server = new grpc.Server();
+	  server.addService(syncer_proto.DatabaseSync.service, {syncData: syncData});
+	  server.bind('0.0.0.0:50051', grpc.ServerCredentials.createInsecure());
+	  server.start();
+	},
+
+};
 
 
 /**
  * Start up the grpc server for people to connect and send
  * data through
- */
 function main() {
   var server = new grpc.Server();
   server.addService(syncer_proto.DatabaseSync.service, {syncData: syncData});
@@ -31,3 +46,4 @@ function main() {
 }
 
 main();
+ */
