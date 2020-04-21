8bdcd9ba973de23a2d6b78e64fd846869dcb0d25
Ivan <inieto1@nmsu.edu>
2020-04-20 20:51:59 -0600

    Finished cleaning up required packages

--- test-project/public/Main.js
+++ test-project/public/Main.js
@@ -1,20 +1,18 @@
 const path = require("path");
-const { server } = require(`file://${path.join(__dirname, "../node/expressServer.js"));
+// const { server } = require(`file://${path.join(__dirname, "../node/expressServer.js"));
 
 const { app, BrowserWindow } = require("electron");
 function createWindow() {
-  
   // Create the browser window.
   win = new BrowserWindow({
     width: 1360,
     height: 800,
-    icon: `file://${path.join(__dirname, "./logo.jpg")}`
+    icon: "./logo.jpg",
   });
 
   win.loadURL(`file://${path.join(__dirname, "../build/index.html")}`);
 
-  server();
-
+  // server();
 }
 app.on("ready", createWindow);
 app.on("window-all-closed", () => {
